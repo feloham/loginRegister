b869494611e22152eacc9c3be7c5387db1ec1c03
pelegtz <pelegtz@gmail.com>
2013-06-29 18:47:36 +0300

    externalized error handling

--- core/functions/general.php
+++ core/functions/general.php
@@ -2,4 +2,13 @@
 function sanitize($username) {
 	return mysql_real_escape_string($username);
 }
+
+function output_errors($errors) {
+	$output = array();
+	foreach($errors as $error) {
+		$output[] = '<li>' . $error . '</li>';
+	}
+	return '<ul>' . implode('', $output) . '</ul>';
+}
+
  ?>
--- login.php
+++ login.php
@@ -1,17 +1,22 @@
 <?php 
 include 'core/init.php';
-include "includes/overall/header.php";
+
 if (empty($_POST) === false)  {
 	$username = $_POST['username'];
 	$password = $_POST['password'];
 
 	if (empty($username) === true || empty($password) === true) {
-		$errors[] = "Please enter User Name and Password.";
+		$errors[] = "Please fill the rewiured fields";
 	} elseif (user_exists($username) === false) {
 		$errors[] = "User Name doesn't exist";
 	} elseif (user_active($username) === false) {
-		$errors[] = "Account is not Activated";
+		$errors[] = "Account is not activated";
 	} else {
+
+		if (strlen($password) > 32) {
+			$errors[] = 'Password is invalid';
+		}
+
 		$login = login($username, $password);
 			if ($login === false) {
 				$errors[] = "This user and password combination is wrong";
@@ -21,9 +26,12 @@ if (empty($_POST) === false)  {
 				exit();
 			}
 	}
-
-	print_r($errors);
 }
+include "includes/overall/header.php";
 
+if (empty($errors) == false) {
+	echo '<h2>We tried to log you in, but...</h2>';
+	echo output_errors($errors);
+}
 include "includes/overall/footer.php";
  ?>
